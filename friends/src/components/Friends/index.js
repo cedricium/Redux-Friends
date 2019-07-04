11ca36464e6265f6e31457135ae37b62bae5c447
Cedricium <amaya.cedric@gmail.com>
2019-07-04 14:57:54 -0700

    Using `getFriends` action creator, get & display friends list
    Also use the `useEffect` React Hook in a functional component in place
    of the `componentDidMount` method in a class component. Hooks ftw!

--- friends/src/components/Friends/index.js
+++ friends/src/components/Friends/index.js
@@ -1,5 +1,35 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { connect } from 'react-redux'
+import styled from 'styled-components'
 
-const Friends = () => <h1>Friends</h1>
+import { getFriends } from '../../actions'
 
-export default Friends
+const Logo = styled.img`
+  width: 100%;
+  max-width: 15rem;
+`
+
+const Friends = (props) => {
+  useEffect(() => {
+    props.getFriends()
+  })
+
+  return (
+    <div>
+      <Logo src="https://upload.wikimedia.org/wikipedia/commons/b/bc/Friends_logo.svg" />
+      {props.friends.map(friend => (
+        <div key={friend.id}>
+          <p><strong>Name: </strong>{friend.name}</p>
+          <p><strong>Age: </strong>{friend.age}</p>
+          <p><strong>Email: </strong>{friend.email}</p>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+const mapStateToProps = state => ({
+  friends: state.friends,
+})
+
+export default connect(mapStateToProps, { getFriends })(Friends)
